3429c1adccad1e919995f47a116f38b566b233e2
JongHun Kim <kjonghun0828@naver.com>
2025-09-01 18:39:54 +0900

    feat : numpy 실습

---
+++ python/numpy_test.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+def main():
+    list1,list2 = [1,2,3],[4,5,6]
+    a,b = np.array(list1),np.array(list2)
+
+    c = a + b
+    d = a - b
+    e = a * b
+    f = a / b
+    print(c,d,e,f)
+    g = a * 2
+    h = b * 2
+    print(g,h)
+    print("a의 자료형 : ",type(a),type(a[0]))
+    print("a의 shape : ",a.shape)
+    print("a의 차원 : ",a.ndim)
+    print("a의 크기 : ",a.size)
+    print("a 데이터 : ",a)
+
+    imgfile = '/home/kjonghun0828/opencv/open_cv_project/data/lenna.bmp'
+    img = cv2.imread(imgfile)
+
+    print('img 자료형 : ', type(img), type(img[0,0]), img.shape, img.ndim, img.size, img.dtype)
+
+main()
---
+++ python/test.py
@@ -0,0 +1,13 @@
+import cv2
+
+def main():
+    print("hello opencv")
+    print(cv2.__version__)
+    imgfile = '/home/kjonghun0828/opencv/open_cv_project/data/lenna.bmp'
+    img = cv2.imread(imgfile)
+    cv2.imshow("lenna img",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
